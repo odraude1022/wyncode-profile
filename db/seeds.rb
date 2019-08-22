05023dc44966e4b87cc8369f718ab510e8b17f81
Eduardo Iglesias <eriglesias@ufl.edu>
2019-08-22 11:48:56 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_list = [
+  ["Eduardo", "Iglesias", "eriglesias@ufl.edu", 32, "password", "password"],
+  ["Rocio", "De Santiago", "rociodes4@hotmail.com", 33, "password", "password"],
+  ["Thayna", "Santana", "thaynamenezes01@gmail.com", 31, "password", "password"],
+  ["Gina", "De Santiago", "gina.gina@gina.gina", 34, "password", "password"]
+]
+
+user_list.each do |first_name, last_name, email, cohort, password, password_confirmation|
+  User.create(first_name: first_name, last_name: last_name, email: email, cohort: cohort, password: password, password_confirmation: password_confirmation);
+end
