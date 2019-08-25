3e230085c20e723bc9611e0cb6c9782ca5b2ce52
Eduardo Iglesias <eriglesias@ufl.edu>
2019-08-25 13:59:40 -0400

    add User bundle to javascript pack

--- app/javascript/packs/app-bundle.js
+++ app/javascript/packs/app-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import Users from '../bundles/Users/components/Users';
+import User from '../bundles/User/components/User'
 
 // This is how react_on_rails can see the Tasks in the browser.
 ReactOnRails.register({
   Users,
+  User,
 });
