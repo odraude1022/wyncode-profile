99a9509a882f8002157cc835539931908df5cf33
Eduardo Iglesias <eriglesias@ufl.edu>
2019-08-25 14:07:56 -0400

    convert user show page to React

---
+++ app/javascript/bundles/User/components/User.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+const User = props => {
+  const {user, current_user} = props
+  return(
+    <div class="wrapper">
+      <div class="user-info">
+        <h1>{user.first_name} {user.last_name}</h1>
+        <h2>C{user.cohort}</h2>
+        <p>Email: {user.email}</p>
+        {user.id === current_user.id &&
+          <a href={`/users/${user.id}/edit`}>Edit Profile</a>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default User
